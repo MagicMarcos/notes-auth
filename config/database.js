c2b50cf950e9f0349b9b7c76892b31b91b811fc0
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-10-23 19:14:43 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+	url: 'mongodb+srv://savageAuth:pIuybFhikT6dREN7@cluster0.qbdrb.mongodb.net/Notes?retryWrites=true&w=majority',
+	dbName: 'notes',
+};
+
+// ;
+
+//mongodb+srv://savageAuth:pIuybFhikT6dREN7@cluster0.qbdrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
